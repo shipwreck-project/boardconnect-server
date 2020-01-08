8f0e511436b53eec92c75534d6965958bd758e72
Kim Bum June <bjbj6363@gmail.com>
2020-01-08 15:12:49 +0900

    :children_crossing: 서버 에러시에 나오는 메시지 수정

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,6 @@ fn main() {
 
     match Iron::new(chain).http("localhost:3000") {
         Ok(_o) => println!("Server is Starting in Port 3000"),
-        Err(e) => println!("Server start failed due to {}", e),
+        Err(e) => println!("Server is failed due to {}", e),
     }
 }
